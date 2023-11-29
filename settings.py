209166b10ddaf29c5c9056d32ddaf656b0bf418f
changgge <changgge@umich.edu>
2023-11-29 16:15:31 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
 )
 
-PARTICIPANT_FIELDS = ['boot']
+PARTICIPANT_FIELDS = ['boot', 'wait_page_arrival']
 SESSION_FIELDS = []
 
 # ISO-639 code
