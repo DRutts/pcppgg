d1dbba82b704f2bc38b81a99e5db81c25de6bd08
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-06 22:15:12 -0500

    Update __init__.py

--- PCP_I2/__init__.py
+++ PCP_I2/__init__.py
@@ -12,7 +12,7 @@ Your app description
 # ======================
 
 class C(BaseConstants):
-    NAME_IN_URL = 'PCP_I'
+    NAME_IN_URL = 'PCP_I2'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1  
 
@@ -44,80 +44,32 @@ class Player(BasePlayer):
 # ======================
 
 class InstructionsPage2_1(Page):
-    form_model = "player"
-    form_fields = ["Q1_1"]
+    timeout_seconds = 60 * 2
     
     @staticmethod
+
     def is_displayed(player: Player):
-        return player.keep == 0
+        return player.participant.vars['boot'] == False 
+
+
+class InstructionsPage2_1(Page):
+    form_model = "player"
+    form_fields = ["Q2_1", "Q2_2"]
+    timeout_seconds = 60 * 3
+    
+    @staticmethod
+
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False
 
     @staticmethod
     def error_message(player: Player, values):
-        solutions = dict(Q1_1=24)
+        solutions = dict(Q2_1=14,
+                         Q2_2=12)
         errors = {name: 'Wrong' for name in solutions if values[name] != solutions[name]}
         if errors:
-            player.num_failed_attempts_1 += 1
-            if player.num_failed_attempts_1 >= 10:
-                player.keep = 1
-            else:
-                return errors
-
-class Instructions1(Page):
-
-    form_model = "player"
-    form_fields = ["Q1a", "Q1b", "Q2a", "Q2b", "Q3a", "Q3b", "Q4a", "Q4b"]
-    
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(Q1a=20,
-                         Q1b=20,
-                         Q2a=32,
-                         Q2b=32,
-                         Q3a=32,
-                         Q3b=23,
-                         Q4a=18,
-                         Q4b=24)
-        errors = {name: 'Wrong' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.num_failed_attempts_1 += 1
-            if player.num_failed_attempts_1 >= 10:
-                player.keep = 1
-            else:
-                return errors
-
-    
-    
-class Instructions2(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-    form_model = "player"
-    form_fields = ["Q5", "Q6", "Q7", "Q8", "Q9"]
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(Q5=14,
-                         Q6=0,
-                         Q7=0,
-                         Q8=-12,
-                         Q9=-45)
-        errors = {name: 'Wrong' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.num_failed_attempts_2 += 1
-            if player.num_failed_attempts_2 >= 10:
-                player.keep = 1
-            else:
-                return errors
-    
-    def before_next_page(player: Player, timeout_happened):
-        player.participant.vars['wait_arrival_time'] = time.time()
-        if player.keep == 1:
-            player.participant.vars['boot'] = True
-        else: 
-            player.participant.vars['boot'] = False
+            return errors
+        
 
 
 
