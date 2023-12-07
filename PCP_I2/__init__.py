c50d811b27c0a6b863a564e21feaef71ce051821
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-06 22:09:51 -0500

    Update __init__.py

--- PCP_I2/__init__.py
+++ PCP_I2/__init__.py
@@ -26,75 +26,8 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    consent = models.FloatField(
-            choices=[[1, 'I consent']],
-            label = '',
-            widget=widgets.RadioSelect
-        )
-    keep = models.IntegerField(initial = 0)
-    incorrect_attempts1 = models.IntegerField(initial = 0)
-    bot_num = models.IntegerField(initial = 0)
-
-    captcha1 = models.StringField(max_length = 200)
-    captcha2 = models.StringField(max_length = 200)
-    incorrect_attempts_captcha1 = models.IntegerField(initial = 0)
-    incorrect_attempts_captcha2 = models.IntegerField(initial = 0)
-    TimeoutCapthca1 = models.BooleanField(initial = False)
-    TimeoutCapthca2 = models.BooleanField(initial = False)
-
-    num_failed_attempts_1 = models.IntegerField(initial=0)
-    num_failed_attempts_2 = models.IntegerField(initial=0)
-    failed_too_many = models.BooleanField(initial=False)
-    TimeoutQ1 = models.BooleanField(initial = False)
-    TimeoutQ2 = models.BooleanField(initial = False)
-    Q1_1 = models.IntegerField()
-    Q2_1 = models.IntegerField()
     Q2_2 = models.IntegerField()
-    Q1a = models.IntegerField(
-        label = "a) What is your income?"
-    )
-    Q1b = models.IntegerField(
-        label = "b) What is the income of the other group members?"
-    )
-    Q2a = models.IntegerField(
-        label = "a) What is your income?"
-    )
-    Q2b = models.IntegerField(
-        label = "b) What is the income of the other group members?"
-    )
-    Q3a = models.IntegerField(
-        label = "a) What is your income if you contribute 0 tokens to the project?"
-    )
-    Q3b = models.IntegerField(
-        label = "b) What is your income if you contribute 15 tokens to the project?"
-    )
-    Q4a = models.IntegerField(
-        label = "a) What is your income if the other group members together contribute a total of 7 tokens to the project?"
-    )
-    Q4b = models.IntegerField(
-        label = "b) What is your income if the other group members together contribute a total of 22 tokens to the project"
-    )
 
-    Q5 = models.IntegerField(
-        min=-50, max=50,
-        label='5) Suppose at the second stage you assign the following deduction points to your three other group members:-9,-5,0. What are the total costs of your assigned deduction points?'
-    )
-    Q6 = models.IntegerField(
-        min=-50, max=50,
-        label='6) What are your costs if you assign a total of 0 points?'
-    )
-    Q7 = models.IntegerField(
-        min=-50, max=50,
-        label='7) By how many Guilders will your income from the first stage be changed if you receive a total of 0 deduction points from the other group members?'
-    )
-    Q8 = models.IntegerField(
-        min=-50, max=50,
-        label='8) By how many Guilders will your income from the first stage be changed if you receive a total of 4 deduction points from the other group members?'
-    )
-    Q9 = models.IntegerField(
-        min=-50, max=50,
-        label='9) By how many Guilders will your income from the first stage be changed if you receive a total of 15 deduction points from the other group members?'
-    )
 
 
 
@@ -110,86 +43,7 @@ class Player(BasePlayer):
 #       PAGE PART
 # ======================
 
-class Consent(Page):
-    form_model = 'player'
-    form_fields = ['consent']
-
-    @staticmethod
-    def consent_error_message(player: Player, value):
-        solutions = dict(consent='I consent')
-        errors = {name: '''You must accept the consent form to proceed with the study''' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            return errors
-
-
-class Captcha1(Page):
-    form_model = 'player'
-    form_fields = ['captcha1']   
-    timeout_seconds = 90
-
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(captcha1='RUNAJIX')
-        errors = {name: '''Please type the characters correctly, case sensitive''' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.incorrect_attempts_captcha1 += 1
-            if player.incorrect_attempts_captcha1 >= 3:
-                player.keep = 1
-            else:
-                return errors
-
-    def before_next_page(player, timeout_happened):
-        if timeout_happened:
-            player.TimeoutCapthca1 = True
-            player.keep = 1
-        else: 
-            player.TimeoutCapthca1 = False
-            
-
-
-class Captcha2(Page):
-    form_model = 'player'
-    form_fields = ['captcha2']
-    timeout_seconds = 90
-
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(captcha2=["eps10 vector", 'eps10 vect0r', "epsio vector", "epsio vect0r", 'eps1o vector', 'eps1o vect0r'])
-        errors = {name: '''Please type the characters correctly, including any numbers, letters, and spaces. Use lowercase.''' for name in solutions if values[name] not in solutions[name]}
-        if errors:
-            player.incorrect_attempts_captcha2 += 1
-            if player.incorrect_attempts_captcha2 >= 3:
-                player.keep = 1
-            else:
-                return errors
-
-    def before_next_page(player, timeout_happened):
-        if timeout_happened:
-            player.TimeoutCapthca2 = True
-            player.keep = 1
-        else: 
-            player.TimeoutCapthca2 = False
-            
-class InstructionsPage1_1(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-class InstructionsPage1_2(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-class InstructionsPage1_3(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-class InstructionsPage1_4(Page):
+class InstructionsPage2_1(Page):
     form_model = "player"
     form_fields = ["Q1_1"]
     
@@ -265,14 +119,8 @@ class Instructions2(Page):
         else: 
             player.participant.vars['boot'] = False
 
-class Elimination(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 1
 
-page_sequence = [Consent,
-                 Captcha1,
-                 Captcha2,
-                 Instructions1, 
-                 Instructions2,
-                 Elimination]
+
+page_sequence = [InstructionsPage2_1,
+                 InstructionsPage2_2
+                ]
