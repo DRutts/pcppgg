d00eb772f304d4f2663e476f8a15e768b69d3ff9
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-26 18:48:13 -0500

    Update __init__.py

--- PCP_P/__init__.py
+++ PCP_P/__init__.py
@@ -61,10 +61,10 @@ class Player(BasePlayer):
 # ======================
 
 def GetPID(player: Player):
-    return 'PunishmentTo{}'.format(player.id_in_group)
+    return 'PunishmentTo{}'.format(player.DispID)
 
 def Punishment_Fields(player: Player):
-    return ['PunishmentTo{}'.format(p.id_in_group) for p in player.get_others_in_group()]
+    return ['PunishmentTo{}'.format(p.DispID) for p in player.get_others_in_group()]
 
 def SetPrelimPayoffs(group: Group):
     players = group.get_players()
@@ -72,12 +72,25 @@ def SetPrelimPayoffs(group: Group):
     group.TotalContribution = sum(contributions)
     group.PGEarnings = group.TotalContribution * C.MULTIPLIER / C.PLAYERS_PER_GROUP
     group.Rounded_PGEarnings = round(group.PGEarnings, 2)
+    
+    IDList = ["A", "B", "C", "D"]
+    P1id = IDList.pop(random.randomint(0,3))
+    IDList_i2 = IDList.remove("P1id")
+    P2id = IDList_i2.pop(random.randomint(0,2))
+    IDList_i3 = IDList_i2.remove("P2id")
+    P3id = IDList_i3.pop(random.randomint(0,1))
+    IDList_i4 = IDList_i3.remove("P3id")
+    P4id = IDList_i4[0]
+    DispIDList = [P1id, P2id, P3id, P4id]
 
     for p in players:
+        p.DispID = DispIDList[p.id_in_group]
         PID = GetPID(p) 
         p.ContributionPercentage = p.Contribution/C.ENDOWMENT * 100
         p.RetainedEndowment = C.ENDOWMENT - p.Contribution
         p.PreliminaryPayoff = C.ENDOWMENT - p.Contribution + group.Rounded_PGEarnings
+
+
 
 
 def SetRevisedPayoffs(group: Group):
