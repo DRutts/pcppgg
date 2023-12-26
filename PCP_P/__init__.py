c30ae4bd33ade2541abb095ef029d382262adbb9
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-26 18:53:12 -0500

    Update __init__.py

--- PCP_P/__init__.py
+++ PCP_P/__init__.py
@@ -75,11 +75,11 @@ def SetPrelimPayoffs(group: Group):
     
     IDList = ["A", "B", "C", "D"]
     P1id = IDList.pop(random.randint(0,3))
-    IDList_i2 = IDList.remove("P1id")
+    IDList_i2 = IDList.remove(P1id)
     P2id = IDList_i2.pop(random.randint(0,2))
-    IDList_i3 = IDList_i2.remove("P2id")
+    IDList_i3 = IDList_i2.remove(P2id)
     P3id = IDList_i3.pop(random.randint(0,1))
-    IDList_i4 = IDList_i3.remove("P3id")
+    IDList_i4 = IDList_i3.remove(P3id)
     P4id = IDList_i4[0]
     DispIDList = [P1id, P2id, P3id, P4id]
 
