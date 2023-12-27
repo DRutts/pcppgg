0046686746ef29aa86b5fe479b740c3b4c17f12e
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-27 00:39:18 -0500

    Update __init__.py

--- PCP_P/__init__.py
+++ PCP_P/__init__.py
@@ -62,10 +62,10 @@ class Player(BasePlayer):
 # ======================
 
 def GetPID(player: Player):
-    return 'PunishmentTo{}'.format(player.DispID)
+    return 'PunishmentTo{}'.format(player.id_in_group)
 
 def Punishment_Fields(player: Player):
-    return ['PunishmentTo{}'.format(p.DispID) for p in player.get_others_in_group()]
+    return ['PunishmentTo{}'.format(p.id_in_group) for p in player.get_others_in_group()]
 
 def SetPrelimPayoffs(group: Group):
     players = group.get_players()
