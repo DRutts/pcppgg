6b686da771d4cb9893bf2fac25fe9688f6dcc5eb
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-26 19:06:07 -0500

    Update __init__.py

--- PCP_N/__init__.py
+++ PCP_N/__init__.py
@@ -44,6 +44,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     PID = models.IntegerField()
+    DispID = models.IntegerField()
     Contribution = models.IntegerField(
         min=0, max=C.ENDOWMENT, label="How much will you contribute?"
     )
