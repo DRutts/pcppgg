9d8dd79aae3d5fee118ef5b5a9b5e365aeb4d653
DRutts <138715987+DRutts@users.noreply.github.com>
2023-12-26 19:11:14 -0500

    Update __init__.py

--- PCP_P/__init__.py
+++ PCP_P/__init__.py
@@ -38,7 +38,7 @@ def make_punishment_field(id_in_group):
 
 class Player(BasePlayer):
     PID = models.IntegerField()
-    DispID = models.IntegerField()
+    DispID = models.StrField()
     Contribution = models.IntegerField(
         min=0, max=20, label="How much will you contribute?"
     )
