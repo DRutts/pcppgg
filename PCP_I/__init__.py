6f919fe4fb37501ec7e3de619d4797e0dbef0fa2
DRutts <138715987+DRutts@users.noreply.github.com>
2024-01-04 20:20:23 -0500

    Update __init__.py

--- PCP_I/__init__.py
+++ PCP_I/__init__.py
@@ -1,7 +1,6 @@
 from otree.api import *
-
 import time
-
+import random
 
 doc = """
 Your app description
@@ -12,90 +11,51 @@ Your app description
 # ======================
 
 class C(BaseConstants):
-    NAME_IN_URL = 'PCP_I'
-    PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1  
+    NAME_IN_URL = 'PCP_P'
+    MULTIPLIER = 1.6
+    PLAYERS_PER_GROUP = 4
+    NUM_ROUNDS = 10
+    ENDOWMENT = 25
+    PUNISHMENT_MULTIPLIER = 3
+    MAX_PUNISHMENT = 10
+    
+
+
 
 class Subsession(BaseSubsession):
     pass
 
 
 class Group(BaseGroup):
-    pass
+    TotalContribution = models.IntegerField()
+    PGEarnings = models.FloatField()
+    Rounded_PGEarnings = models.FloatField()
 
-
+def make_punishment_field(id_in_group):
+        return models.IntegerField(
+            min=0, max=C.MAX_PUNISHMENT, label="Deduction assigned to Player {}".format(id_in_group)
+        )
 
 class Player(BasePlayer):
-    consent = models.FloatField(
-            choices=[[1, 'I consent']],
-            label = '',
-            widget=widgets.RadioSelect
-        )
-    keep = models.IntegerField(initial = 0)
-    incorrect_attempts1 = models.IntegerField(initial = 0)
-    bot_num = models.IntegerField(initial = 0)
-
-    captcha1 = models.StringField(max_length = 200)
-    captcha2 = models.StringField(max_length = 200)
-    incorrect_attempts_captcha1 = models.IntegerField(initial = 0)
-    incorrect_attempts_captcha2 = models.IntegerField(initial = 0)
-    TimeoutCapthca1 = models.BooleanField(initial = False)
-    TimeoutCapthca2 = models.BooleanField(initial = False)
-
-    num_failed_attempts_1 = models.IntegerField(initial=0)
-    num_failed_attempts_2 = models.IntegerField(initial=0)
-    failed_too_many = models.BooleanField(initial=False)
-    TimeoutQ1 = models.BooleanField(initial = False)
-    TimeoutQ2 = models.BooleanField(initial = False)
-    Q1_1 = models.IntegerField()
-    Q2_1 = models.IntegerField()
-    Q2_2 = models.IntegerField()
-    Q1a = models.IntegerField(
-        label = "a) What is your income?"
+    PID = models.IntegerField()
+    DispID = models.IntegerField()
+    Timeout_C = models.IntegerField(initial = 0)
+    Timeout_P = models.IntegerField(initial = 0)
+    Contribution = models.IntegerField(
+        min=0, max=20, label="How much will you contribute?"
     )
-    Q1b = models.IntegerField(
-        label = "b) What is the income of the other group members?"
-    )
-    Q2a = models.IntegerField(
-        label = "a) What is your income?"
-    )
-    Q2b = models.IntegerField(
-        label = "b) What is the income of the other group members?"
-    )
-    Q3a = models.IntegerField(
-        label = "a) What is your income if you contribute 0 tokens to the project?"
-    )
-    Q3b = models.IntegerField(
-        label = "b) What is your income if you contribute 15 tokens to the project?"
-    )
-    Q4a = models.IntegerField(
-        label = "a) What is your income if the other group members together contribute a total of 7 tokens to the project?"
-    )
-    Q4b = models.IntegerField(
-        label = "b) What is your income if the other group members together contribute a total of 22 tokens to the project"
-    )
-
-    Q5 = models.IntegerField(
-        min=-50, max=50,
-        label='5) Suppose at the second stage you assign the following deduction points to your three other group members:-9,-5,0. What are the total costs of your assigned deduction points?'
-    )
-    Q6 = models.IntegerField(
-        min=-50, max=50,
-        label='6) What are your costs if you assign a total of 0 points?'
-    )
-    Q7 = models.IntegerField(
-        min=-50, max=50,
-        label='7) By how many Guilders will your income from the first stage be changed if you receive a total of 0 deduction points from the other group members?'
-    )
-    Q8 = models.IntegerField(
-        min=-50, max=50,
-        label='8) By how many Guilders will your income from the first stage be changed if you receive a total of 4 deduction points from the other group members?'
-    )
-    Q9 = models.IntegerField(
-        min=-50, max=50,
-        label='9) By how many Guilders will your income from the first stage be changed if you receive a total of 15 deduction points from the other group members?'
-    )
-
+    ContributionPercentage = models.FloatField()
+    RetainedEndowment = models.IntegerField()
+    PreliminaryPayoff = models.FloatField()
+    PunishmentTo1 = make_punishment_field(1)
+    PunishmentTo2 = make_punishment_field(2)
+    PunishmentTo3 = make_punishment_field(3)
+    PunishmentTo4 = make_punishment_field(4)
+    TotalPunishmentsFrom = models.IntegerField()
+    TotalPunishmentsTo = models.IntegerField()
+    PayoffReduction = models.IntegerField()
+    RevisedPayoff = models.FloatField()
+    Rounded_RevisedPayoff = models.FloatField()
 
 
 
@@ -103,6 +63,55 @@ class Player(BasePlayer):
 #    CALCULATION PART
 # ======================
 
+def ShuffleID(group: Group):
+    players = group.get_players()
+    IDList = [1, 2, 3, 4]
+    P1id = IDList.pop(random.randint(0,3))
+    P2id = IDList.pop(random.randint(0,2))
+    P3id = IDList.pop(random.randint(0,1))
+    P4id = IDList[0]
+    DispIDList = [P1id, P2id, P3id, P4id]
+    
+    for p in players:
+        p.DispID = DispIDList[p.id_in_group - 1]
+
+def GetPID(player: Player):
+    return 'PunishmentTo{}'.format(player.DispID)
+
+def Punishment_Fields(player: Player):
+    return ['PunishmentTo{}'.format(p.DispID) for p in player.get_others_in_group()]
+
+def SetPrelimPayoffs(group: Group):
+    players = group.get_players()
+    contributions = [p.Contribution for p in players]
+    group.TotalContribution = sum(contributions)
+    group.PGEarnings = group.TotalContribution * C.MULTIPLIER / C.PLAYERS_PER_GROUP
+    group.Rounded_PGEarnings = round(group.PGEarnings, 2)
+
+    for p in players:
+
+        PID = GetPID(p) 
+        p.ContributionPercentage = p.Contribution/C.ENDOWMENT * 100
+        p.RetainedEndowment = C.ENDOWMENT - p.Contribution
+        p.PreliminaryPayoff = C.ENDOWMENT - p.Contribution + group.Rounded_PGEarnings
+
+
+    
+
+
+
+
+def SetRevisedPayoffs(group: Group):
+    players = group.get_players()
+    for p in players: 
+        PID = GetPID(p) 
+        punishments_received = [getattr(other, PID) for other in p.get_others_in_group()]
+        p.TotalPunishmentsTo = sum(punishments_received)
+        punishments_sent = [getattr(p, field) for field in Punishment_Fields(p)]
+        p.TotalPunishmentsFrom = sum(punishments_sent)
+        p.PayoffReduction = C.PUNISHMENT_MULTIPLIER*p.TotalPunishmentsTo
+        p.RevisedPayoff = p.PreliminaryPayoff - p.TotalPunishmentsFrom - p.PayoffReduction
+        p.Rounded_RevisedPayoff = round(p.RevisedPayoff, 2)
 
 
 
@@ -110,169 +119,108 @@ class Player(BasePlayer):
 #       PAGE PART
 # ======================
 
-class Consent(Page):
-    form_model = 'player'
-    form_fields = ['consent']
+class InstructionsWaitPage(WaitPage):
+    after_all_players_arrive = ShuffleID
+    body_text = "Please wait for the other players to join. The waiting time will take at most 6 minutes."    
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == 1 and player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+
+class Inter_RoundWaitPage(WaitPage):
+    after_all_players_arrive = ShuffleID
+    body_text = "Please wait for the other players to join. The waiting time will take at most 30 seconds."    
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number >= 2 and player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+
+class ContributionPage(Page):
+    form_model = "player"
+    form_fields = ["Contribution"]
+    timeout_seconds = 60
 
     @staticmethod
-    def consent_error_message(player: Player, value):
-        solutions = dict(consent='I consent')
-        errors = {name: '''You must accept the consent form to proceed with the study''' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            return errors
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+    
+    def before_next_page(player, timeout_happened):
+        if timeout_happened:
+            player.Contribution = random.randint(0,20)
+            player.Timeout_C = 1
 
 
-class Captcha1(Page):
+
+class ResultsWaitPage(WaitPage):
+    body_text = "Please wait for the other players to join. The waiting time will take at most 1 minute."
+    after_all_players_arrive = SetPrelimPayoffs
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+
+
+class PreliminaryResults(Page):
+    timeout_seconds = 30
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+    
+
+class InformationScreen(Page):
+    timeout_seconds = 30
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+
+    def vars_for_template(player: Player):
+        return dict(
+            other_players=player.get_others_in_group(),
+        )
+    
+
+class PunishmentPage(Page):
     form_model = 'player'
-    form_fields = ['captcha1']   
-    timeout_seconds = 90
+    get_form_fields = Punishment_Fields
+    timeout_seconds = 60 * 2
 
     @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(captcha1='RUNAJIX')
-        errors = {name: '''Please type the characters correctly, case sensitive''' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.incorrect_attempts_captcha1 += 1
-            if player.incorrect_attempts_captcha1 >= 3:
-                player.keep = 1
-            else:
-                return errors
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
+    
+    def vars_for_template(player: Player):
+        return dict(
+            other_players=player.get_others_in_group(),
+        )
 
     def before_next_page(player, timeout_happened):
         if timeout_happened:
-            player.TimeoutCapthca1 = True
-            player.keep = 1
-        else: 
-            player.TimeoutCapthca1 = False
-            
+            player.PunishmentTo1 = 0
+            player.PunishmentTo2 = 0
+            player.PunishmentTo3 = 0
+            player.PunishmentTo4 = 0
+            player.Timeout_P = 1
 
+class PunishmentWaitPage(WaitPage):
+    after_all_players_arrive = SetRevisedPayoffs
+    body_text = "Please wait for the other players to join. The waiting time will take at most 3 minutes."
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
 
-class Captcha2(Page):
-    form_model = 'player'
-    form_fields = ['captcha2']
-    timeout_seconds = 90
+class RevisedResults(Page):
+    timeout_seconds = 30
 
     @staticmethod
     def is_displayed(player: Player):
-        return player.keep == 0
+        return player.participant.vars['boot'] == False and player.participant.vars['WTL'] == False
 
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(captcha2=["eps10 vector", 'eps10 vect0r', "epsio vector", "epsio vect0r", 'eps1o vector', 'eps1o vect0r'])
-        errors = {name: '''Please type the characters correctly, including any numbers, letters, and spaces. Use lowercase.''' for name in solutions if values[name] not in solutions[name]}
-        if errors:
-            player.incorrect_attempts_captcha2 += 1
-            if player.incorrect_attempts_captcha2 >= 3:
-                player.keep = 1
-            else:
-                return errors
 
-    def before_next_page(player, timeout_happened):
-        if timeout_happened:
-            player.TimeoutCapthca2 = True
-            player.keep = 1
-        else: 
-            player.TimeoutCapthca2 = False
-            
-class InstructionsPage1_1(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
 
-class InstructionsPage1_2(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
 
-class InstructionsPage1_3(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-class InstructionsPage1_4(Page):
-    form_model = "player"
-    form_fields = ["Q1_1"]
-    
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(Q1_1=24)
-        errors = {name: 'Wrong' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.num_failed_attempts_1 += 1
-            if player.num_failed_attempts_1 >= 10:
-                player.keep = 1
-            else:
-                return errors
-
-class Instructions1(Page):
-
-    form_model = "player"
-    form_fields = ["Q1a", "Q1b", "Q2a", "Q2b", "Q3a", "Q3b", "Q4a", "Q4b"]
-    
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(Q1a=20,
-                         Q1b=20,
-                         Q2a=32,
-                         Q2b=32,
-                         Q3a=32,
-                         Q3b=23,
-                         Q4a=18,
-                         Q4b=24)
-        errors = {name: 'Wrong' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.num_failed_attempts_1 += 1
-            if player.num_failed_attempts_1 >= 10:
-                player.keep = 1
-            else:
-                return errors
-
-    
-    
-class Instructions2(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 0
-    form_model = "player"
-    form_fields = ["Q5", "Q6", "Q7", "Q8", "Q9"]
-    @staticmethod
-    def error_message(player: Player, values):
-        solutions = dict(Q5=14,
-                         Q6=0,
-                         Q7=0,
-                         Q8=-12,
-                         Q9=-45)
-        errors = {name: 'Wrong' for name in solutions if values[name] != solutions[name]}
-        if errors:
-            player.num_failed_attempts_2 += 1
-            if player.num_failed_attempts_2 >= 10:
-                player.keep = 1
-            else:
-                return errors
-    
-    def before_next_page(player: Player, timeout_happened):
-        player.participant.vars['wait_arrival_time'] = time.time()
-        if player.keep == 1:
-            player.participant.vars['boot'] = True
-        else: 
-            player.participant.vars['boot'] = False
-
-class Elimination(Page):
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.keep == 1
-
-page_sequence = [Consent,
-                 Captcha1,
-                 Captcha2,
-                 Instructions1, 
-                 Instructions2,
-                 Elimination]
+page_sequence = [InstructionsWaitPage,
+                 Inter_RoundWaitPage,
+                 ContributionPage, 
+                 ResultsWaitPage, 
+                 PreliminaryResults,
+                 InformationScreen,
+                 PunishmentPage, 
+                 PunishmentWaitPage, 
+                 RevisedResults]
