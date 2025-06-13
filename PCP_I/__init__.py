3ea37f1cbad72a9ec67af29a06c8a4b339e83a2e
DRutts <138715987+DRutts@users.noreply.github.com>
2025-06-13 14:01:04 -0400

    Update __init__.py

--- PCP_I/__init__.py
+++ PCP_I/__init__.py
@@ -230,8 +230,8 @@ class GroupingWaitPage(WaitPage):
     
 
 class InstructionsPage2_1(Page):
-    #timeout_seconds = 1
-    timeout_seconds = 60 * 2
+    timeout_seconds = 1
+    #timeout_seconds = 60 * 2
     
     @staticmethod
 
@@ -244,8 +244,8 @@ class InstructionsPage2_1(Page):
 class InstructionsPage2_2(Page):
     form_model = "player"
     form_fields = ["Q2_1", "Q2_2"]
-    #timeout_seconds = 1
-    timeout_seconds = 60 * 3
+    timeout_seconds = 1
+    #timeout_seconds = 60 * 3
     
     @staticmethod
 
@@ -292,8 +292,8 @@ class Inter_RoundWaitPage_P(WaitPage):
 class ContributionPage_N(Page):
     form_model = "player"
     form_fields = ["Contribution"]
-    #timeout_seconds = 1
-    timeout_seconds = 60
+    timeout_seconds = 1
+    #timeout_seconds = 60
 
     @staticmethod
     def is_displayed(player: Player):
@@ -342,8 +342,8 @@ class ResultsWaitPage_P(WaitPage):
 
 
 class PreliminaryResults(Page):
-    #timeout_seconds = 1
-    timeout_seconds = 30
+    timeout_seconds = 1
+    #timeout_seconds = 30
     @staticmethod
     def is_displayed(player: Player):
         return player.participant.vars['boot'] == False and player.Remove == 0
@@ -353,8 +353,8 @@ class PreliminaryResults(Page):
 
 
 class InformationScreen_N(Page):
-    #timeout_seconds = 1
-    timeout_seconds = 30
+    timeout_seconds = 1
+    #timeout_seconds = 30
     @staticmethod
     def is_displayed(player: Player):
         return player.round_number <= 10 and player.participant.vars['boot'] == False and player.Remove == 0
